4e6d68fe7d2f88125579cdc7c29f49d3acf1dd2b
Jorge Martinez <jorgemartinez.ans@gmail.com>
2019-12-17 12:17:12 +1300

    fix firebase api key

--- src/main.js
+++ src/main.js
@@ -21,17 +21,15 @@ axios.defaults.baseURL = 'https://app-2brains-test.herokuapp.com/api';
 
 Vue.config.productionTip = false
 
-//const firebaseConfig = require("C:/Users/Jorge/Desktop/respaldo/proyectos/node/fireBaseConfig.json");
-
 const firebaseConfig = {
-  "apiKey": "AIzaSyCddhra4Z3WYLUaCOuR9Y_hZXGA6IzRKZA",
-  "authDomain": "brainstest-94ad6.firebaseapp.com",
-  "databaseURL": "https://brainstest-94ad6.firebaseio.com",
-  "projectId": "brainstest-94ad6",
-  "storageBucket": "brainstest-94ad6.appspot.com",
-  "messagingSenderId": "958480747722",
-  "appId": "1:958480747722:web:4d159b50b55709f5fef833",
-  "measurementId": "G-JZE7SHVVM3"
+  "apiKey": process.env.VUE_APP_FB_APIKEY,
+  "authDomain": process.env.VUE_APP_FB_AUTHDOMAIN,
+  "databaseURL": process.env.VUE_APP_FB_DATABASE_URL,
+  "projectId": process.env.VUE_APP_FB_PROJECT_ID,
+  "storageBucket": process.env.VUE_APP_FB_STORAGE_BUCKET,
+  "messagingSenderId": process.env.VUE_APP_FB_MESSAGIN_SEND_ID,
+  "appId": process.env.VUE_APP_FB_APP_ID,
+  "measurementId": process.env.VUE_APP_FB_MEASUREMENT_ID
 }
 
 // Initialize Firebase
